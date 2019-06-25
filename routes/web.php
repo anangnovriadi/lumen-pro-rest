c494513a304d9a1c52d50f9ddb34e21877e0a6cf
anang <anangnov99@gmail.com>
2019-06-25 16:28:27 +0700

    random key for .env

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+// Key Random
+$router->get('/key', function() {
+    return str_random(32);
+});
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
